b266be7645bc8f95461fb74a94681d6e55c499a1
roblivesinottawa <59901422+roblivesinottawa@users.noreply.github.com>
2020-07-14 11:55:18 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,40 @@
+// process.stdout.write("hello ")
+// process.stdout.write("world \n\n\n")
+
+
+const questions = [
+    'what is your name?',
+    'what would you rather be doing?',
+    'what is your preferred programming language?'
+];
+
+const ask = (i = 0) => {
+    process.stdout.write(`\n\n\n ${questions[i]}`);
+    process.stdout.write(`  >  `);
+};
+
+ask();
+
+const answers = []
+process.stdin.on('data', data => {
+    answers.push(data.toString().trim());
+
+    if (answers.length < questions.length) {
+        ask(answers.length)
+    } else {
+        process.exit()
+    }
+});
+
+process.on('exit', () => {
+
+    const [name, activity, lang ] = answers;
+    console.log(`
+    
+
+    thanks for your answers.
+
+    Go ${activity} ${name} you can write ${lang} code later!!!
+    
+    `)
+})
